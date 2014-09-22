cbfe40f60726b146ba3d9b63ee8871e4ac542ab9
Sarkis Peha <sarkispeha@gmail.com>
2014-09-22 17:26:50 -0600

    setting up clock containers

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 $(document).on('ready', function() {
+
+var outer_shell = $('<div class="outer-shell"></div>');
+var inner_shell = $('<div class="inner-shell"</div>');
+
+	// $('.container').css({
+	// 	'height': '100%',
+	// 	'width': '100%'
+	// })
+
+	// outer_shell.css({
+	// 	'height': 400,
+	// 	'width': 650,
+	// 	backgroundColor: 'darkgrey',
+	// 	'margin': '20%'
+	// })
+	$('.container').append(outer_shell);
   
+	// inner_shell.css({
+	// 	'height': 350,
+	// 	'width': 600,
+	// 	backgroundColor: 'black'
+	// })
+	outer_shell.append(inner_shell);
+
 });
