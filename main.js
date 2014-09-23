c82fffafca55967a0f0c411c6e08475291dde601
Sarkis Peha <sarkispeha@gmail.com>
2014-09-23 14:43:52 -0600

    finally got the clock to work

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 $(document).on('ready', function() {
 
+
+
 var outer_shell = $('<div class="outer-shell"></div>');
 var inner_shell = $('<div class="inner-shell"</div>');
 var amfm = $('<div class="am-fm">AM<br>FM</div>');
@@ -7,9 +9,23 @@ var clock_screen = $('<div class="clock-screen"></div>');
 var auto = $('<div class="au-to">AUTO</div>');
 var pm = $('<div class="pm">PM</div>');
 var pm_indicator = $('<div class="pm-indicator"</div>');
-var clock_text = $('<div class="clock-text">12:17</div>');
+var clock_text = $('<div class="clock-text"></div>');
 var am_freq = $('<div class="am-freq">53 60 70 90 110 140 170</div>');
 var fm_freq = $('<div class="fm-freq">89 92 96 102 106 108</div>');
+// var time = $('<p class="time"></p>')
+
+
+var getTime = function(){
+	var currentDate = new Date();
+	var minutes = currentDate.getMinutes();
+	var hours = currentDate.getHours();
+
+	clock_text.text(hours + ':' + minutes);
+};
+
+// var time = setInterval(function(){getTime}, 1000);
+setInterval(getTime, 1000);
+
 
 
 
@@ -23,5 +39,16 @@ clock_screen.append(pm_indicator);
 clock_screen.append(clock_text);
 inner_shell.append(am_freq);
 inner_shell.append(fm_freq);
+
+// var mins = '';
+// if(minutes < 10) {
+// 	var mins = '0' + minutes;
+// 	return mins;
+// }else{
+// 	return minutes;
+// };
+
+
+
 
 });
