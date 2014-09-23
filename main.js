67b724418b87a82846f69a126ab3f811fd03c222
Sarkis Peha <sarkispeha@gmail.com>
2014-09-22 22:30:50 -0600

    styling of clock mostly completed

--- main.js
+++ main.js
@@ -2,25 +2,26 @@ $(document).on('ready', function() {
 
 var outer_shell = $('<div class="outer-shell"></div>');
 var inner_shell = $('<div class="inner-shell"</div>');
+var amfm = $('<div class="am-fm">AM<br>FM</div>');
+var clock_screen = $('<div class="clock-screen"></div>');
+var auto = $('<div class="au-to">AUTO</div>');
+var pm = $('<div class="pm">PM</div>');
+var pm_indicator = $('<div class="pm-indicator"</div>');
+var clock_text = $('<div class="clock-text">12:17</div>');
+var am_freq = $('<div class="am-freq">53 60 70 90 110 140 170</div>');
+var fm_freq = $('<div class="fm-freq">89 92 96 102 106 108</div>');
 
-	// $('.container').css({
-	// 	'height': '100%',
-	// 	'width': '100%'
-	// })
 
-	// outer_shell.css({
-	// 	'height': 400,
-	// 	'width': 650,
-	// 	backgroundColor: 'darkgrey',
-	// 	'margin': '20%'
-	// })
-	$('.container').append(outer_shell);
-  
-	// inner_shell.css({
-	// 	'height': 350,
-	// 	'width': 600,
-	// 	backgroundColor: 'black'
-	// })
-	outer_shell.append(inner_shell);
+
+$('.container').append(outer_shell);
+outer_shell.append(inner_shell);
+inner_shell.append(clock_screen);
+inner_shell.append(amfm);
+inner_shell.append(auto);
+inner_shell.append(pm);
+clock_screen.append(pm_indicator);
+clock_screen.append(clock_text);
+inner_shell.append(am_freq);
+inner_shell.append(fm_freq);
 
 });
