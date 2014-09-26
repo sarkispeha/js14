1f9b3bd8c02dc14e4dd4cf2176c5be61aa780a58
Sarkis Peha <sarkispeha@gmail.com>
2014-09-26 15:29:39 -0600

    added PM indicator to function and corrected 0 digit placeholders

--- main.js
+++ main.js
@@ -9,21 +9,33 @@ var clock_screen = $('<div class="clock-screen"></div>');
 var auto = $('<div class="au-to">AUTO</div>');
 var pm = $('<div class="pm">PM</div>');
 var pm_indicator = $('<div class="pm-indicator"</div>');
+var pm_indicator_active = $('<div class="active-pm-indicator"</div>')
 var clock_text = $('<div class="clock-text"></div>');
 var am_freq = $('<div class="am-freq">53 60 70 90 110 140 170</div>');
 var fm_freq = $('<div class="fm-freq">89 92 96 102 106 108</div>');
-// var time = $('<p class="time"></p>')
-
 
 var getTime = function(){
 	var currentDate = new Date();
 	var minutes = currentDate.getMinutes();
 	var hours = currentDate.getHours();
+	
+	if (hours > 12) {
+		$('.active-pm-indicator').show();
+	}
 
+	if ((hours % 12) < 10) {
+		hours = '0' + hours %12;
+	}else{hours = hours % 12}
+
+
+	if(minutes < 10) {
+		minutes = '0' + minutes;
+	};
 	clock_text.text(hours + ':' + minutes);
+
+
 };
 
-// var time = setInterval(function(){getTime}, 1000);
 setInterval(getTime, 1000);
 
 
@@ -36,17 +48,10 @@ inner_shell.append(amfm);
 inner_shell.append(auto);
 inner_shell.append(pm);
 clock_screen.append(pm_indicator);
+clock_screen.append(pm_indicator_active);
 clock_screen.append(clock_text);
 inner_shell.append(am_freq);
 inner_shell.append(fm_freq);
-
-// var mins = '';
-// if(minutes < 10) {
-// 	var mins = '0' + minutes;
-// 	return mins;
-// }else{
-// 	return minutes;
-// };
 
 
 
